0efb629ff95020dcd8cd2802e9e687db486be9cb
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-04-01 18:08:23 -0600

    Fixed formatting

--- src/utils/commands/assets/cat/PinkQween.md.js
+++ src/utils/commands/assets/cat/PinkQween.md.js
@@ -1,90 +1,183 @@
+// export default `
+// # Hanna Skairipa - Full-stack JavaScript developer\n
+// <!--<p align="center">\n
+// <img alt="Profile picture" src="https://c4.wallpaperflare.com/wallpaper/640/145/78/anime-girl-headphones-short-hair-playing-games-wallpaper-preview.jpg" width="1000" />\n
+// </p>--!>\n\n
+
+// <!-- ![snake gif](https://github.com/PinkQween/PinkQween/blob/output/github-contribution-grid-snake.gif) --!>\n\n
+
+// <p align="center">\n
+// <img alt="Trophies" src="https://github-profile-trophy.vercel.app/?username=pinkQween&theme=radical&rank=-UNKNOWN,-B,-C,-UNKOWN&column=-1&margin-w=5&margin-h=5">\n
+// </p>\n\n
+
+// Welcome to my GitHub profile! I'm Hanna Skairipa aka PinkQween, I'm a full stack developer usually working with JavaScript or TypeScript.\n\n
+
+// <br />\n
+// <br />\n\n
+
+// <p align="center" style="display:flex;flexDirection:column">\n
+// <img alt="PinkQweens's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=PinkQween&show_icons=true&theme=radical">\n
+// </p>\n\n
+
+// <p align="center" style="display:flex;flexDirection:column">\n
+// <img alt="PinkQweens's Top Langs" src="https://github-readme-stats.vercel.app/api/top-langs/?username=PinkQween&theme=radical&layout=compact">\n
+// </p>\n\n
+
+// <!-- ## Projects\n\n
+
+// Here are some of the projects I have worked on:\n\n
+
+// ### 1. Skairipa Weather App\n\n
+
+// ![Skairipa Weather App](https://github.com/PinkQween/SkairipaWeatherApp)\n\n
+
+// The Skairipa Weather App is a beautiful and intuitive weather application that provides real-time weather information for various locations. It includes features such as detailed weather forecasts, interactive maps, and customizable notifications. The app is built using Swift and utilizes APIs to fetch weather data.\n\n
+
+// ### 2. Skairipa Notes\n\n
+
+// ![Skairipa Notes](https://github.com/PinkQween/SkairipaNotes)\n\n
+
+// Skairipa Notes is a simple yet powerful note-taking app designed for iOS. It allows users to create, organize, and sync their notes across multiple devices. The app supports rich text editing, attachments, and seamless iCloud integration. It is developed using Swift and CoreData for data management.\n\n
+
+// ### 3. Skairipa Fitness Tracker\n\n
+
+// ![Skairipa Fitness Tracker](https://github.com/PinkQween/SkairipaFitnessTracker)\n\n
+
+// Skairipa Fitness Tracker is a comprehensive fitness and activity tracking app for iOS. It enables users to set fitness goals, track their workouts, monitor calories burned, and analyze their progress over time. The app incorporates gamification elements to keep users motivated. It is built with Swift and integrates with HealthKit for health data retrieval. -->\n\n
+
+// ## Skills\n\n
+
+// - Swift\n
+// - Swift UI\n
+// - iOS SDK\n
+// - UIKit\n
+// - AutoLayout\n
+// - Core Data\n
+// - RESTful APIs\n
+// - Git\n
+// - UI/UX Design\n
+// - Javascript\n
+// - React.js\n
+// - CSS\n
+// - SCSS\n
+// - SASS\n
+// - React Native\n
+// - HTML\n
+// - Unity\n
+// - C\n
+// - C++\n
+// - C#\n
+// - Express\n
+// - Redux\n
+// - Linux\n
+// - Bash\n\n
+
+// ## Diplomas && Certificates\n\n
+
+// - Excellence in Language and Literature\n
+// - Certificate of Continuation\n
+// - VR design with Unity game engine\n
+// - Certificate of completion SWiFT\n
+// - Game Design and Development 101 with Unreal Engine\n\n
+
+// ## Contact\n\n
+
+// Feel free to reach out to me if you have any questions, suggestions, or collaboration opportunities. You can contact me via email at therealsquireboone@gmail.com\n\n
+
+// Looking forward to collaborating and creating amazing applications together!\n\n
+
+// > "Making daily life easier."
+// `
+
 export default `
-# Hanna Skairipa - Full-stack JavaScript developer\n
-<!--<p align="center">\n
-<img alt="Profile picture" src="https://c4.wallpaperflare.com/wallpaper/640/145/78/anime-girl-headphones-short-hair-playing-games-wallpaper-preview.jpg" width="1000" />\n
-</p>--!>\n\n
 
-<!-- ![snake gif](https://github.com/PinkQween/PinkQween/blob/output/github-contribution-grid-snake.gif) --!>\n\n
+# Hanna Skairipa - Full-stack JavaScript developer
+<!--<p align="center">
+<img alt="Profile picture" src="https://c4.wallpaperflare.com/wallpaper/640/145/78/anime-girl-headphones-short-hair-playing-games-wallpaper-preview.jpg" width="1000" />
+</p>--!>
 
-<p align="center">\n
-<img alt="Trophies" src="https://github-profile-trophy.vercel.app/?username=pinkQween&theme=radical&rank=-UNKNOWN,-B,-C,-UNKOWN&column=-1&margin-w=5&margin-h=5">\n
-</p>\n\n
+<!-- ![snake gif](https://github.com/PinkQween/PinkQween/blob/output/github-contribution-grid-snake.gif) --!>
 
-Welcome to my GitHub profile! I'm Hanna Skairipa aka PinkQween, I'm a full stack developer usually working with JavaScript or TypeScript.\n\n
+<p align="center">
+<img alt="Trophies" src="https://github-profile-trophy.vercel.app/?username=pinkQween&theme=radical&rank=-UNKNOWN,-B,-C,-UNKOWN&column=-1&margin-w=5&margin-h=5">
+</p>
 
-<br />\n
-<br />\n\n
+Welcome to my GitHub profile! I'm Hanna Skairipa aka PinkQween, I'm a full stack developer usually working with JavaScript or TypeScript.
 
-<p align="center" style="display:flex;flexDirection:column">\n
-<img alt="PinkQweens's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=PinkQween&show_icons=true&theme=radical">\n
-</p>\n\n
+<br />
+<br />
 
-<p align="center" style="display:flex;flexDirection:column">\n
-<img alt="PinkQweens's Top Langs" src="https://github-readme-stats.vercel.app/api/top-langs/?username=PinkQween&theme=radical&layout=compact">\n
-</p>\n\n
+<p align="center" style="display:flex;flexDirection:column">
+<img alt="PinkQweens's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=PinkQween&show_icons=true&theme=radical">
+</p>
 
-<!-- ## Projects\n\n
+<p align="center" style="display:flex;flexDirection:column">
+<img alt="PinkQweens's Top Langs" src="https://github-readme-stats.vercel.app/api/top-langs/?username=PinkQween&theme=radical&layout=compact">
+</p>
 
-Here are some of the projects I have worked on:\n\n
+<!-- ## Projects
 
-### 1. Skairipa Weather App\n\n
+Here are some of the projects I have worked on:
 
-![Skairipa Weather App](https://github.com/PinkQween/SkairipaWeatherApp)\n\n
+### 1. Skairipa Weather App
 
-The Skairipa Weather App is a beautiful and intuitive weather application that provides real-time weather information for various locations. It includes features such as detailed weather forecasts, interactive maps, and customizable notifications. The app is built using Swift and utilizes APIs to fetch weather data.\n\n
+![Skairipa Weather App](https://github.com/PinkQween/SkairipaWeatherApp)
 
-### 2. Skairipa Notes\n\n
+The Skairipa Weather App is a beautiful and intuitive weather application that provides real-time weather information for various locations. It includes features such as detailed weather forecasts, interactive maps, and customizable notifications. The app is built using Swift and utilizes APIs to fetch weather data.
 
-![Skairipa Notes](https://github.com/PinkQween/SkairipaNotes)\n\n
+### 2. Skairipa Notes
 
-Skairipa Notes is a simple yet powerful note-taking app designed for iOS. It allows users to create, organize, and sync their notes across multiple devices. The app supports rich text editing, attachments, and seamless iCloud integration. It is developed using Swift and CoreData for data management.\n\n
+![Skairipa Notes](https://github.com/PinkQween/SkairipaNotes)
 
-### 3. Skairipa Fitness Tracker\n\n
+Skairipa Notes is a simple yet powerful note-taking app designed for iOS. It allows users to create, organize, and sync their notes across multiple devices. The app supports rich text editing, attachments, and seamless iCloud integration. It is developed using Swift and CoreData for data management.
 
-![Skairipa Fitness Tracker](https://github.com/PinkQween/SkairipaFitnessTracker)\n\n
+### 3. Skairipa Fitness Tracker
 
-Skairipa Fitness Tracker is a comprehensive fitness and activity tracking app for iOS. It enables users to set fitness goals, track their workouts, monitor calories burned, and analyze their progress over time. The app incorporates gamification elements to keep users motivated. It is built with Swift and integrates with HealthKit for health data retrieval. -->\n\n
+![Skairipa Fitness Tracker](https://github.com/PinkQween/SkairipaFitnessTracker)
 
-## Skills\n\n
+Skairipa Fitness Tracker is a comprehensive fitness and activity tracking app for iOS. It enables users to set fitness goals, track their workouts, monitor calories burned, and analyze their progress over time. The app incorporates gamification elements to keep users motivated. It is built with Swift and integrates with HealthKit for health data retrieval. -->
 
-- Swift\n
-- Swift UI\n
-- iOS SDK\n
-- UIKit\n
-- AutoLayout\n
-- Core Data\n
-- RESTful APIs\n
-- Git\n
-- UI/UX Design\n
-- Javascript\n
-- React.js\n
-- CSS\n
-- SCSS\n
-- SASS\n
-- React Native\n
-- HTML\n
-- Unity\n
-- C\n
-- C++\n
-- C#\n
-- Express\n
-- Redux\n
-- Linux\n
-- Bash\n\n
+## Skills
 
-## Diplomas && Certificates\n\n
+- Swift
+- Swift UI
+- iOS SDK
+- UIKit
+- AutoLayout
+- Core Data
+- RESTful APIs
+- Git
+- UI/UX Design
+- Javascript
+- React.js
+- CSS
+- SCSS
+- SASS
+- React Native
+- HTML
+- Unity
+- C
+- C++
+- C#
+- Express
+- Redux
+- Linux
+- Bash
 
-- Excellence in Language and Literature\n
-- Certificate of Continuation\n
-- VR design with Unity game engine\n
-- Certificate of completion SWiFT\n
-- Game Design and Development 101 with Unreal Engine\n\n
+## Diplomas && Certificates
 
-## Contact\n\n
+- Excellence in Language and Literature
+- Certificate of Continuation
+- VR design with Unity game engine
+- Certificate of completion SWiFT
+- Game Design and Development 101 with Unreal Engine
 
-Feel free to reach out to me if you have any questions, suggestions, or collaboration opportunities. You can contact me via email at therealsquireboone@gmail.com\n\n
+## Contact
 
-Looking forward to collaborating and creating amazing applications together!\n\n
+Feel free to reach out to me if you have any questions, suggestions, or collaboration opportunities. You can contact me via email at therealsquireboone@gmail.com
+
+Looking forward to collaborating and creating amazing applications together!
 
 > "Making daily life easier."
+
 `
